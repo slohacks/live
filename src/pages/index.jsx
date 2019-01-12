a9921842811741eca71f9a0555cb5eb463de3899
Jumer-Caragay <jumer.caragay@gmail.com>
2019-01-12 18:50:17 -0400

    refactor(src/): setup src for live site

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Tracks from '../components/Tracks';
-import History from '../components/History';
-import Baker from '../components/Baker';
-// import Schedule from '../components/Schedule';
-import FAQ from '../components/FAQ';
-import Sponsor from '../components/Sponsor';
-import CTA from '../components/CTA';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Tracks from './Tracks';
 import styles from './index.module.css';
 
 const IndexPage = () => (
   <div className={styles.layout}>
-    <Hero />
-    <About />
-    <Tracks />
-    <History />
-    <Baker />
-    {/* <Schedule /> */}
-    <FAQ />
-    <Sponsor />
-    <CTA />
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/tracks" component={Tracks} />
+      </Switch>
+    </HashRouter>
   </div>
 );
 export default IndexPage;
